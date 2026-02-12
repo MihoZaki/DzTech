a1693eaa62698296b9e0966bd3f9548324432ab4
MihoZaki <mihoubi.zakaria.dev@gmail.com>
2026-02-12 11:30:36 +0100

    fix: allowed file ext check no uses regex

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -6,6 +6,7 @@ import (
 	"mime/multipart"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 	"sync"
 
@@ -42,27 +43,47 @@ func NewLocalStorage(basePath, publicPath string, allowedTypes []string, maxSize
 func (ls *LocalStorage) UploadFile(file multipart.File, fileHeader *multipart.FileHeader) (string, error) {
 	ls.mutex.Lock()
 	defer ls.mutex.Unlock()
+
+	// --- Size Check ---
 	if fileHeader.Size > ls.maxSize {
 		return "", fmt.Errorf("file size %d exceeds maximum allowed size %d", fileHeader.Size, ls.maxSize)
 	}
+
+	// --- Type Check ---
 	ext := strings.ToLower(filepath.Ext(fileHeader.Filename))
 	if ext != ".jpg" && ext != ".jpeg" && ext != ".png" && ext != ".gif" && ext != ".webp" {
 		return "", fmt.Errorf("file type %s is not allowed", ext)
 	}
-	filename := fmt.Sprintf("%s%s", uuid.New().String(), ext)
-	fullPath := filepath.Join(ls.basePath, filename)
+	// --- Generate Unique Filename ---
+	originalFilenameWithoutExt := strings.TrimSuffix(fileHeader.Filename, ext)
+	// Sanitize the original name if necessary (remove/replace problematic characters)
+	santizedFileNameWithoutExt := sanitize(originalFilenameWithoutExt)
+	uuidPart := uuid.New().String()
+	uniqueFilename := fmt.Sprintf("%s_%s%s", santizedFileNameWithoutExt, uuidPart, ext)
+	// ---
+
+	// --- Save File ---
+	fullPath := filepath.Join(ls.basePath, uniqueFilename)
 
 	dst, err := os.Create(fullPath)
 	if err != nil {
 		return "", fmt.Errorf("failed to create destination file %s: %w", fullPath, err)
 	}
 	defer dst.Close()
+
 	_, err = io.Copy(dst, file)
 	if err != nil {
+		// Clean up the partially created file on error
 		os.Remove(fullPath)
 		return "", fmt.Errorf("failed to copy uploaded file to %s: %w", fullPath, err)
 	}
-	return fmt.Sprintf("%s/%s", strings.TrimSuffix(ls.publicPath, "/"), filename), nil
+	// ---
+
+	// --- Generate Public URL ---
+	publicURL := fmt.Sprintf("%s/%s", strings.TrimSuffix(ls.publicPath, "/"), uniqueFilename)
+	// ---
+
+	return publicURL, nil
 }
 
 func (ls *LocalStorage) DeleteFile(fileURL string) error {
@@ -78,4 +99,10 @@ func (ls *LocalStorage) DeleteFile(fileURL string) error {
 }
 func (ls *LocalStorage) GetFileURL(filename string) string {
 	return fmt.Sprintf("%s/%s", strings.TrimSuffix(ls.publicPath, "/"), filename)
+}
+func sanitize(filename string) string {
+	// Remove or replace characters that might be problematic in filenames
+	// This is a basic example, might need expansion based on OS/filesystem requirements
+	re := regexp.MustCompile(`[^a-zA-Z0-9._-]`)
+	return re.ReplaceAllString(filename, "_")
 }
